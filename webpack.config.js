f6b4f9c8c6a43ec8b6f19f95c472ef0347d0618e
Ryan <ryanker1024@gmail.com>
2020-10-17 21:31:47 +0800

    vmsg.simplify.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports = {
+    mode: "production",
+    // mode: "development",
+    entry: "./vmsg.simplify.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "vmsg.min.js",
+        library: "vmsg",
+        // libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {
+                test: /.ts$/,
+                use: ["babel-loader", "ts-loader"],
+                exclude: [path.resolve(__dirname, "node_modules")]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
+    // devtool: "inline-source-map"
+}
